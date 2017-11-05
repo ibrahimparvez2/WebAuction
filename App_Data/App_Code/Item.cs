a5bbe8d1752f1f9048fbebcf7e476cf273d24204
ibrahimparvez2 <31848160+ibrahimparvez2@users.noreply.github.com>
2017-11-05 20:20:11 +0000

    Add files via upload

---
+++ App_Data/App_Code/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAuction.App_Code
+{
+    public class Item
+    {
+        public string name;
+        public string description;
+        public double yourBid;
+        public double currentBid;
+        public double reservedPrice;
+        public double highestBid;
+
+        //public Item(string name, string description, double currentBid, double reservedPrice, double highestBid)
+        //{
+        //    this.name = name;
+        //    this.description = description;
+        //    this.currentBid = currentBid;
+        //    this.reservedPrice = reservedPrice;
+        //    this.highestBid = highestBid;
+
+        //}
+    }
+
+}
+
+    
