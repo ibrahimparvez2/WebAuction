0c5b19a81a66fcc0f1058dd608bc3087507e9530
ibrahimparvez2 <31848160+ibrahimparvez2@users.noreply.github.com>
2017-11-05 20:24:05 +0000

    Add files via upload

---
+++ default.aspx
@@ -0,0 +1,144 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebAuction._default"%>
+<%--<%=_watch.name%>
+<%=_watch.highestBid%>
+<%=_watch.currentBid%>--%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+  <meta name="viewport" content="width=device-width, initial-scale=1"/>
+     <link rel="stylesheet" href="Styles/bootstrap.css" />
+     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans|Tangerine" rel="stylesheet"/>
+     <link rel="stylesheet" href="Styles/styles.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title></title>    
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager> 
+        <asp:Timer ID="Timer1" runat="server" OnTick="Timer1_Tick" Interval="60000">
+        </asp:Timer>
+     
+            <h1> Vintage Watch Auction</h1>
+        <hr />
+            
+        
+        <h3 id="listing">Current selection available for bidding</h3>
+        <div class="container content">
+              <div class="row">
+                   <div class="col-md-4">
+                    <asp:Image ID="Image1" runat="server" Height="200" Width="200" ImageUrl="~/rolextp.png"/> 
+                </div>
+                  <div class="col-md-4">
+                  <asp:Image ID="Image2" runat="server" Height="200" Width="200" ImageUrl="~/Chanel1.png"/>
+                   </div>
+                  <div class="col-md-4">
+             <asp:Image ID="Image3" runat="server" Height="200" Width="200" ImageUrl="~/omegasm.png"/>
+                </div>
+            </div>
+            <div class ="row">
+                  <div class="col-md-4">
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo">Rolex Oyster Description</button>
+                        <div id="demo" class="collapse">
+                            Outstanding 1979 Rolex Oyster Perpetual Date Gents Vintage Watch. 12-Hour Dial, COSC Certified Chronometer, Date Indicator, Screwback Case
+                        </div>
+                </div> 
+                 <div class="col-md-4">
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2"> Chanel J12 Description</button>
+                        <div id="demo2" class="collapse">
+                            Chanel J12 White Unisex Watch
+                            Crisp, contemporary, and effortlessly chic, this Chanel J12 White Unisex Watch exudes the unrivalled confidence, style,
+                             and sophistication that we have come to expect from this iconic brand.
+                        </div>
+                </div> 
+                 <div class="col-md-4">
+                    <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo3">Omega Sea Master Description</button>
+                        <div id="demo3" class="collapse">
+                            This Seamaster 300 has a blue enamel dial with 18K white gold hands coated with "vintage" Super-LumiNova. The polished ceramic bezel ring has an 850-grade platinum Liquidmetal® diving scale.
+                            The 41 mm brushed and polished 950 platinum case is presented on a matching bracelet.
+                        </div>
+                </div> 
+        </div>
+    </div>
+
+       
+       <div class="container"> 
+           <div class="jumbotron">
+        <h3>CURRENT TIME REMAINING:</h3> 
+             <asp:Label ID="Label5" runat="server" Font-Size="Medium" Font-Underline="true" Text="Time remaining"></asp:Label>
+              
+        <hr />
+        <p>Please select an option from the dropdown list to start bidding 
+         <asp:ListBox ID="ListBox1" runat="server" Height="51px" Width="152px" ForeColor="Black">
+        </asp:ListBox>
+       </p>
+       
+        </div>
+         </div>
+        <div class="container">
+            <div class="jumbotron" id="bidTicker">
+                  <h3>Please enter the amount you would like to bid</h3>
+            <asp:updatepanel runat="server">
+     <ContentTemplate>
+         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         <asp:Button ID="bid" runat="server" Height="33px" Text="BID(£)" Width="109px" OnClick="bid_Click" ForeColor="Black" CausesValidation="true"/>
+         <asp:TextBox ID="inputB" runat="server" BackColor="White" ForeColor="#000099" ToolTip="Enter a amount in £x.xx"></asp:TextBox>
+         &nbsp;&nbsp;
+          
+        <asp:RegularExpressionValidator runat="server" id="bidAmount"
+            ControlToValidate="inputB"
+            ErrorMessage="Only numeric allowed." ForeColor="Red"
+            ValidationExpression="^[0-9]+(\.[0-9]{1,2})?$" Text="*Please enter in Currency Format Please"/>
+         
+         <br />
+         
+         <asp:RangeValidator runat="server" id="bidRange"
+           ControlToValidate ="inputB"
+           MinimumValue="1"
+           MaximumValue="999999"
+           Type="Double"
+           SetFocusOnError="true"
+           Text="Please enter an amount between 1-999,999" /> 
+                
+            <br />
+     
+                 <asp:Label ID="Label1" runat="server"></asp:Label>
+     
+                 <div>
+                     <updatepanel> 
+                         <h3>**Current Bid Status**</h3>
+                     </updatepanel>
+                 </div>
+                    <asp:Label ID="_label2" runat="server" Text="_Label2"></asp:Label>
+     
+                 <br />
+                    <asp:Label ID="label4" runat="server" Text="label4"></asp:Label>
+                 <br />
+                     <asp:Label ID="label6" runat="server" Text="Label"></asp:Label>
+                 <asp:Timer ID="Timer2" runat="server" Interval="15000" OnTick="Timer2_Tick"></asp:Timer>
+             </ContentTemplate>
+            </asp:updatepanel>
+         </div>
+   </div>  
+        <div class="container">
+            <div class ="jumbotron">
+
+            <asp:Panel ID="Panel1" Height ="200px" Width="81%" runat ="server" style="margin-left: 61px; margin-top: 18px">
+            
+                <asp:Label  ID="EndAuction" runat="server" Font-Size="Large" Font-Bold="True" Font-Names="Arial" CssClass="text-success"></asp:Label>
+                 <br />
+                 <asp:Label  ID="EndAuction1" runat="server" Text="" Font-Size="Large" Font-Bold="True" Font-Names="Arial" CssClass="text-success"></asp:Label>
+                <br />
+                <asp:Label  ID="EndAuction2" runat="server" Text="" Font-Size="Large" Font-Bold="True" Font-Names="Arial" CssClass="text-success"></asp:Label>
+
+                <asp:Label  ID="EndAuction3" runat="server" Text="" Font-Size="Large" Font-Bold="True" Font-Names="Arial" CssClass="text-success"></asp:Label>
+        </asp:Panel>
+            </div>
+        </div>
+
+      
+    </form>
+</body>
+</html>
---
+++ default.aspx.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebAuction.App_Code;
+using System.Timers;
+
+
+namespace WebAuction
+{
+    public partial class _default : System.Web.UI.Page
+    {
+
+        protected Item _watch;
+        protected Item _watch2;
+        protected Item _watch3;
+
+
+        //public void InitTimer(int minutes)
+        //{
+        //    Session.Add("timer", minutes);
+        //    Timer1.Enabled = true;
+
+
+        //}
+        int currentTime = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                //{
+                //    myAuction = Initialise(myAuction);
+                //}
+                Session["_watch"] = new Item();
+                Item _watch = Session["_Watch"] as Item;
+
+                _watch.name = "Rolex";
+                _watch.description = "Vintage 1970s Rolex Oyster Perpetual";
+                _watch.reservedPrice = 300;
+                _watch.yourBid = 0;
+                _watch.currentBid = 0;
+                _watch.highestBid = 200;
+
+
+                Session["_watch2"] = new Item();
+                Item _watch2 = Session["_Watch2"] as Item;
+
+                _watch2.name = "Chanel";
+                _watch2.description = "Chanel J12 White Unisex Watch";
+                _watch2.reservedPrice = 500;
+                _watch2.yourBid = 0;
+                _watch2.currentBid = 0;
+                _watch2.highestBid = 900;
+
+                Session["_watch3"] = new Item();
+                Item _watch3 = Session["_Watch3"] as Item;
+
+                _watch3.name = "Omega";
+                _watch3.description = "Omega SeaMaster 300";
+                _watch3.reservedPrice = 500;
+                _watch3.yourBid = 0;
+                _watch3.currentBid = 0;
+                _watch3.highestBid = 4000;
+
+
+                ListBox1.Items.Add(_watch.name);
+                ListBox1.Items.Add(_watch2.name);
+                ListBox1.Items.Add(_watch3.name);
+
+                //int currentTime = Convert.ToInt32(Session["timer"]);
+                if (currentTime != 2)
+                {
+                    Session.Add("timer", 2);
+                    Timer1.Enabled = true;
+                    //InitTimer(3); // or whatever initial value is
+                    int minutes = (int)Session["timer"];
+                    Label5.Text = minutes.ToString() + " minutes left to BID";
+                }
+
+                _label2.Text = "\n" + _watch.description + " Current highest bid: £" + _watch.highestBid;
+                label4.Text = "\n" + _watch2.description + " Current highest bid: £" + _watch2.highestBid;
+                label6.Text =  "\n" + _watch3.description + " Current highest bid: £" + _watch3.highestBid;
+            }
+        }
+
+
+        protected void bid_Click(object sender, EventArgs e)
+        {
+            if (bidAmount.IsValid)
+            {
+
+
+                switch (ListBox1.SelectedValue)
+                {
+
+                    case ("Rolex"):
+
+                        Item _watch = Session["_watch"] as Item;
+
+                        if (double.Parse(inputB.Text) > _watch.yourBid) {
+                            _watch.yourBid = double.Parse(inputB.Text);
+
+                        }
+                        else
+                        {
+                            Label1.Text = "You have not increase your current bid for this item please try again";
+                            return;
+                        }
+                        
+                        if (_watch.yourBid > _watch.highestBid)
+                        {
+                            _watch.highestBid = _watch.yourBid;
+
+                                Label1.Text = " Your current bid of £" + _watch.yourBid + " for the " + _watch.description + " has just been submitted you are the current HIGHEST Bidder";
+
+                                _label2.Text = _watch.description + " Current highest bid: £" + _watch.highestBid;
+                        }
+
+                        else
+                        {
+
+                            Label1.Text = " Your current bid of £" + _watch.yourBid + " for the " + _watch.description + " has just been submitted you are NOT the highest bidder.";
+                        }
+                        
+
+                        return;
+
+
+                    case ("Chanel"):
+
+                        Item _watch2 = Session["_watch2"] as Item;
+
+
+                        if (double.Parse(inputB.Text) > _watch2.yourBid)
+                        {
+                            _watch2.yourBid = double.Parse(inputB.Text);
+
+                        }
+                        else
+                        {
+                            Label1.Text = "You have not increase your current bid for this item please try again";
+                            return;
+                        }
+
+
+                        if (_watch2.yourBid > _watch2.highestBid)
+                        {
+                            _watch2.highestBid = _watch2.yourBid;
+
+                            Label1.Text = "Your current bid of £" + _watch2.yourBid + " for the " + _watch2.description + " has just been submitted you are the current HIGHEST Bidder";
+
+                            label4.Text = _watch2.description + " Current highest bid: £" + _watch2.highestBid;
+
+                        }
+                        else
+                        {
+
+                            Label1.Text = " Your current bid of £" + _watch2.yourBid + " for the " + _watch2.description + " has just been submitted you are NOT the highest bidder.";
+                        }
+
+
+                        return;
+
+                    case ("Omega"):
+
+                        Item _watch3 = Session["_watch3"] as Item;
+
+                        if (double.Parse(inputB.Text) > _watch3.yourBid)
+                        {
+                            _watch3.yourBid = double.Parse(inputB.Text);
+
+                        }
+                        else
+                        {
+                            Label1.Text = "You have not increase your current bid for this item please try again";
+                            return;
+                        }
+
+
+                        if (_watch3.yourBid > _watch3.highestBid)
+                        {
+                            _watch3.highestBid = _watch3.yourBid;
+
+
+                            Label1.Text = "Your current bid of £" + _watch3.yourBid + " for the " + _watch3.description + " has been submitted you are the current HIGHEST bidder";
+
+                            label6.Text = _watch3.description + " Current highest bid: £" + _watch3.highestBid;
+
+                        }
+
+                        else
+                        {
+
+                            Label1.Text = " Your current bid of £" + _watch3.yourBid + " for the " + _watch3.description + " has just been submitted you are NOT the highest bidder.";
+                        }
+
+                        return;
+                }
+            }
+
+            else
+            {
+                Label1.Text = "Please Enter in Currency Format £x.xx";
+            }
+        }
+        
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            int minutes = (int)Session["timer"];
+            minutes--;
+            if (minutes > 0)
+            {
+                Session["timer"] = minutes;
+                Label5.Text = minutes.ToString() + " minute left to BID";
+
+            }
+            else if (minutes == 0)
+            {
+                Timer1.Enabled = false;
+                Timer2.Enabled = false;
+                bid.Enabled = false;
+                Label5.Text = "The Auction has now closed Thanks";
+                Item _watch = Session["_watch"] as Item;
+                Item _watch2 = Session["_watch2"] as Item;
+                Item _watch3 = Session["_watch3"] as Item;
+
+                if (_watch.yourBid == _watch.highestBid)
+                {
+                    EndAuction.ForeColor = System.Drawing.Color.DimGray;
+                    EndAuction.Text = "Congrats your bid of £" +_watch.yourBid + " was the winning BID for the Vintage Rolex Oyster Perpetual";
+                    Panel1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+
+
+                }
+
+                if (_watch2.yourBid == _watch2.highestBid)
+                {
+                    EndAuction.ForeColor = System.Drawing.Color.Fuchsia;
+                    EndAuction1.Text = "Congrats your bid of £" + _watch2.yourBid + " was the winning BID for the Chanel J12 White Unisex Watch";
+                    Panel1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+
+                }
+
+                if (_watch3.yourBid == _watch3.highestBid)
+                {
+                    EndAuction.ForeColor = System.Drawing.Color.DimGray;
+                    EndAuction2.Text = "Congrats your bid of £" + _watch3.yourBid + " was the winning BID for the Omega SeaMaster";
+                    Panel1.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+
+                }
+
+                else if(_watch.yourBid < _watch.highestBid && _watch2.yourBid < _watch2.highestBid && _watch3.yourBid < _watch3.highestBid)
+                {
+                    EndAuction3.ForeColor = System.Drawing.Color.DimGray;
+                    EndAuction3.Text = "Unfortunately you were outbidded this time!!";
+
+                }
+
+            }
+
+        }
+
+        protected void Timer2_Tick(object sender, EventArgs e)
+        {
+            
+            Item _watch = Session["_Watch"] as Item;
+            Item _watch2 = Session["_watch2"] as Item;
+            Item _watch3 = Session["_watch3"] as Item;
+            Random random = new Random();
+
+                int randomValue = random.Next(1000, 3000);
+                int randomValue2 = random.Next(3000, 5000);
+                int randomValue3 = random.Next(25000, 60000);
+                _watch.currentBid = randomValue;
+                _watch2.currentBid = randomValue2;
+                _watch3.currentBid = randomValue3;
+
+            if (_watch.currentBid > _watch.highestBid)
+            {
+                _watch.highestBid = _watch.currentBid;
+                _label2.Text = _watch.description + " Current highest bid : £" + _watch.highestBid;
+            }
+
+            if (_watch2.currentBid > _watch2.highestBid)
+            {
+                _watch2.highestBid = _watch2.currentBid;
+                label4.Text = _watch2.description + " Current highest bid: £" + _watch2.highestBid;
+            }
+
+            if (_watch3.currentBid > _watch3.highestBid)
+            {
+                _watch3.highestBid = _watch3.currentBid;
+                label6.Text = _watch3.description + " Current highest bid: £" + _watch3.highestBid;
+            }
+
+        }
+    }
+
+}
+
+
+
+
+//static string Populate(List<Item> newAuction)
+//{
+//    string name = "";
+//    List<string> names = new List<string>();
+
+//    foreach (Item i in newAuction)
+//    {
+//        names.Add(i.name);
+//    }
+
+//    name = names.First();
+//    return name;
+
+//}
